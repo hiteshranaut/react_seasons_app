7c8407091f2088c9e74097d7323f93a2f0dd8539
hiteshranaut <hiteshranaut92@gmail.com>
2019-09-18 14:24:52 +0530

    second push

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+	summer: {
+		text: 'lets hit the beach',
+		iconName: 'sun'
+	},
+	winter: {
+		text: 'lets hit the beach',
+		icon: 'snowflake'
+	}
+};
+
+const getSeason = (lat , month) => {
+	if (month > 2 && month < 9) {
+	return	lat > 0 ? 'summer' : 'winter';
+	} else {
+		return	lat > 0 ? 'winter' : 'summer';
+	}
+
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat , new Date().getMonth());
+    const { text , iconName } = seasonConfig[season];
+    
+
+
+
+
+	return (
+		<div className={`${season} season-display`}>
+		<i className={`icon-left ${iconName} massive icon`} />
+	<h1>{text}</h1>
+	<i className={`icon-right ${iconName} massive icon`} />
+	</div>
+		);
+	
+}
+
+export default SeasonDisplay;
